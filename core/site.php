4b91ed9ec63e615eea9617b27d5a9494f0f4e501
Benedict <benedict_wallis@yahoo.co.uk>
2015-11-06 10:09:07 +0100

    Added Open Graph

--- core/site.php
+++ core/site.php
@@ -7,29 +7,56 @@ namespace PressGang;
  *
  * @package PressGang
  */
-class Site extends \TimberSite {
-
+class Site extends \TimberSite
+{
     public $keywords;
     public $logo;
     public $copyright;
+    public $open_graph;
 
     /**
      *__construct
      *
      * @param string|int $site_name_or_id
      */
-    function __construct( $site_name_or_id = null ) {
+    function __construct($site_name_or_id = null)
+    {
         parent::__construct($site_name_or_id);
-        $this->init();
+
+        // add custom params
+        $this->keywords = apply_filters('site_keywords', implode(', ', array_map(function ($tag) { return $tag->name;}, get_tags(array('orderby' => 'count', 'order' => 'DESC', 'number' => 20)))));
+        $this->logo = apply_filters('site_logo', get_theme_mod('logo'));
+        $this->copyright = apply_filters('site_copyright', get_theme_mod('copyright'));
+        $this->add_open_graph();
     }
 
     /**
-     * init
+     * add_open_graph
+     *
+     * Add custom open_graph params
      *
      */
-    protected function init() {
-        $this->keywords = apply_filters('site_keywords', implode(', ', array_map(function ($tag) { return $tag->name; }, get_tags(array('orderby' => 'count', 'order' => 'DESC', 'number' => 20)))));
-        $this->logo = apply_filters('site_logo', get_theme_mod('logo'));
-        $this->copyright = apply_filters('site_copyright', get_theme_mod('copyright'));
+    protected function add_open_graph() {
+
+        global $wp;
+
+        $this->open_graph = array();
+
+        $post = new \TimberPost();
+
+        $img = has_post_thumbnail($post->id)
+            ? wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'medium')[0]
+            : (get_theme_mod('og_img')
+                ? get_theme_mod('og_img')
+                : esc_url(get_theme_mod('logo')));
+
+        $type = is_author() ? 'profile' : (is_single() ? 'article' : 'website');
+
+        $this->open_graph['site_name'] = esc_attr(apply_filters('og_site_name', get_bloginfo()));
+        $this->open_graph['title'] = esc_attr(apply_filters('og_title', $this->title));
+        $this->open_graph['description'] = esc_attr(apply_filters('og_description', $this->description));
+        $this->open_graph['type'] = esc_attr(apply_filters('og_type', $type));
+        $this->open_graph['url'] = rtrim(esc_url(apply_filters('og_url', home_url(add_query_arg(array(), $wp->request)))), '/') . '/'; // slash fixes Facebook Debugger "Circular Redirect Path"
+        $this->open_graph['image'] = esc_url(apply_filters('og_image', $img));
     }
 }
