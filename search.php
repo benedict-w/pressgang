22871262c7a00b57b5e685501d27548241d73861
Benedict <benedict_wallis@yahoo.co.uk>
2015-11-21 13:57:44 +0100

    Updated search.php context

--- search.php
+++ search.php
@@ -1,10 +1,11 @@
 <?php
 /**
- * Template Name: Search Page
+ * Description: Search Page
  */
 
 $templates = array('search.twig', 'archive.twig', 'index.twig');
 $context = Timber::get_context();
-$context['title'] = __("Search results for %s", get_search_query());
+$context['page_title'] = __("Search results for %s", get_search_query());
 $context['posts'] = Timber::get_posts();
+$context['pagination'] = Timber::get_pagination();
 Timber::render($templates, $context);
