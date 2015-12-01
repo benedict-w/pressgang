4d005d6978884bde6a6de8e8debf6d2a7ede8eec
Benedict <benedict_wallis@yahoo.co.uk>
2015-12-01 10:44:48 +0100

    Added text-domain to search text

--- search.php
+++ search.php
@@ -1,11 +1,12 @@
 <?php
 /**
  * Description: Search Page
+ *
  */
 
 $templates = array('search.twig', 'archive.twig', 'index.twig');
 $context = Timber::get_context();
-$context['page_title'] = __("Search results for %s", get_search_query());
+$context['page_title'] = sprintf(__("Search results for '%s'", THEMENAME), get_search_query());
 $context['posts'] = Timber::get_posts();
 $context['pagination'] = Timber::get_pagination();
 Timber::render($templates, $context);
