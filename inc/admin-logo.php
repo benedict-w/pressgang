bdfb2654d6d85b2c2e3d0268c19a8d9f68282474
Benedict <benedict_wallis@yahoo.co.uk>
2016-06-05 16:42:31 +0200

    Fixed admin logo after Wordpress 4.5.2 Upgrade

--- inc/admin-logo.php
+++ inc/admin-logo.php
@@ -23,12 +23,12 @@ class AdminLogo {
         if($logo = esc_url(get_theme_mod('logo'))) : ?>
             <style type="text/css">
                 .login h1 a {
-                    background-image: url(<?php echo $logo; ?>);
+                    background-image: url(<?php echo $logo; ?>) !important;
                     padding-bottom: 30px;
-                    width: auto;
-                    height: auto;
-                    -webkit-background-size: contain;
-                    background-size: contain;
+                    width: auto !important;
+                    height: auto !important;
+                    -webkit-background-size: contain !important;
+                    background-size: contain !important;
                 }
             </style>
         <?php endif;
