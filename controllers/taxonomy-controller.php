eb2891e02b327c3f8a0d1ff4c8e078d74bca9dfe
Benedict <benedict_wallis@yahoo.co.uk>
2016-09-06 15:58:30 +0100

    Added taxonomy controller

---
+++ controllers/taxonomy-controller.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace PressGang;
+
+require_once 'base-controller.php';
+
+/**
+ * Class TaxonomyController
+ *
+ * @package PressGang
+ */
+class TaxonomyController extends PostsController {
+
+    protected $taxonomy;
+
+    /**
+     * __construct
+     *
+     * PageController constructor
+     *
+     * @param string $template
+     */
+    public function __construct($template = 'taxonomy.twig') {
+        parent::__construct($template);
+    }
+
+    /**
+     * get_post
+     *
+     * @return mixed
+     */
+    protected function get_taxonomy()
+    {
+        if (empty($this->taxonomy)) {
+            $this->taxonomy = new \TimberTerm(get_queried_object());
+        }
+
+        return $this->taxonomy;
+    }
+
+    /**
+     * get_context
+     *
+     * @return mixed
+     */
+    protected function get_context()
+    {
+        parent::get_context();
+        $this->context['taxonomy'] = $this->get_taxonomy();
+
+        return $this->context;
+    }
+}
