f7d2f8f6b1c8ef10854cf7bc3d567430eec7d087
Benedict <benedict_wallis@yahoo.co.uk>
2016-09-20 15:11:58 +0100

    Added authors widget

---
+++ widgets/authors.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Pressgang;
+
+/**
+ * Class Authors
+ *
+ * @package Pressgang
+ */
+class Authors extends \Pressgang\Widget {
+
+    /**
+     * widget
+     *
+     * @param array $args
+     * @param array $instance
+     */
+    public function widget($args, $instance) {
+
+        $authors = get_users(array(
+            'orderby' => 'post_count',
+            'order'   => 'DESC',
+            'who'     => 'authors',
+        ));
+
+        foreach ($authors as $i => &$author) {
+            if (count_user_posts($author->ID)) {
+                $author = new \TimberUser($author);
+            }
+            else {
+                unset($authors[$i]);
+            }
+        }
+
+        $instance['authors'] = $authors;
+
+        parent::widget($args, $instance);
+    }
+}
+
+register_widget('PressGang\Authors');
