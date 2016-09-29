2d5f0952c394fc2ab84d08c89d15bd78af4d8856
Benedict <benedict_wallis@yahoo.co.uk>
2016-09-29 16:16:19 +0100

    Added document.ready()

--- js/src/custom/gallery.js
+++ js/src/custom/gallery.js
@@ -1,17 +1,20 @@
 (function($) {
 
-    /**
-     * Setup magnific-popup on the gallery
-     *
-     * See - http://dimsemenov.com/plugins/magnific-popup/
-     *     - https://github.com/dimsemenov/Magnific-Popup/
-     */
-    $('.magnific-popup').magnificPopup({
-        delegate: 'a',
-        type: 'image',
-        gallery: {
-            enabled: true
-        }
+    $(function(){
+
+        /**
+         * Setup magnific-popup on the gallery
+         *
+         * See - http://dimsemenov.com/plugins/magnific-popup/
+         *     - https://github.com/dimsemenov/Magnific-Popup/
+         */
+        $('.magnific-popup').magnificPopup({
+            delegate: 'a',
+            type: 'image',
+            gallery: {
+                enabled: true
+            }
+        });
     });
 
 })(jQuery);
