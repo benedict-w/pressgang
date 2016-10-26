8330ed065e622fe2358a628e00960b63b190062b
Benedict <benedict_wallis@yahoo.co.uk>
2016-10-26 15:47:30 +0100

    Fixed OG Title / Desc / URL for taxonomy and term pages

--- inc/opengraph.php
+++ inc/opengraph.php
@@ -25,13 +25,7 @@ class OpenGraph {
 
         $post = new \TimberPost();
 
-        $title = $post->title;
-
-        $description = is_single()
-            ? str_replace('', "'", $post->get_preview(60, true, false, true))
-            : get_bloginfo('description', 'display');
-
-        $img = has_post_thumbnail($post->id)
+        $img = has_post_thumbnail($post->ID)
             ? wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'large')[0]
             : (get_theme_mod('og_img')
                 ? get_theme_mod('og_img')
@@ -39,7 +33,28 @@ class OpenGraph {
 
         $type = is_author() ? 'profile' : (is_single() ? 'article' : 'website');
 
-        $url = get_permalink();
+        $description = get_bloginfo('description', 'display');
+
+        if (is_tax()) {
+            $url = get_term_link(get_query_var('term'), get_query_var('taxonomy'));
+            $title = single_term_title('', false);
+            if ($temp = term_description(get_queried_object(), get_query_var('taxonomy'))) {
+                $description = $temp;
+            }
+        }
+        elseif(is_post_type_archive()) {
+            $url = get_post_type_archive_link(get_query_var('post_type'));
+            $title = get_the_archive_title();
+            if ($temp = get_the_archive_description()) {
+                $description = $temp;
+            }
+        }
+        else {
+            $url = get_permalink();
+            $title = get_the_title();
+            $description = $post->get_preview(60, true, false, true);
+        }
+
         $url = rtrim(esc_url(apply_filters('og_url', $url)));
         if (!substr($url, -1) === '/') {
             $url .= '/'; // slash fixes Facebook Debugger "Circular Redirect Path"
