a579dd9a6f210a641d66f23123badd84da96d261
Benedict <benedict_wallis@yahoo.co.uk>
2016-11-14 17:37:16 +0000

    Refactored search.php

--- search.php
+++ search.php
@@ -1,4 +1,5 @@
 <?php
 
 $posts_controller = new \PressGang\PostsController('search.twig');
+$posts_controller->context['page_title'] = sprintf(__("Search results for '%s'", THEMENAME), get_search_query());
 $posts_controller->render();
