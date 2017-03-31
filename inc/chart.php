b9847e8be6f624454ba1f18297b6b82f487ccd0a
Benedict <benedict_wallis@yahoo.co.uk>
2017-03-31 09:30:25 +0100

    Added Google Charts

---
+++ inc/chart.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace PressGang;
+
+/**
+ * Class Charts
+ *
+ * https://cdnjs.com/libraries/Chart.js
+ *
+ * @package PressGang
+ */
+class Chart
+{
+    public  static function register ()
+    {
+        // enqueue chart.js
+        Scripts::$scripts['slick'] = array(
+            'src' => 'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.bundle.min.js',
+            'ver' => '2.5.0',
+            'in_footer' => true
+        );
+
+        // enqueue pressgang's chart.js
+        Scripts::$scripts['pressgang-chart'] = array(
+            'src' => get_template_directory_uri() . '/js/src/custom/slick.js',
+            'deps' => array('chart', 'jquery'),
+            'ver' => '0.1',
+            'in_footer' => true
+        );
+    }
+
+
+}
---
+++ js/src/custom/google-charts.js
@@ -0,0 +1,10 @@
+(function($) {
+
+    $(function() {
+
+        google.charts.load('current', {packages: ['corechart']});
+        google.charts.setOnLoadCallback(drawChart);
+
+    });
+
+})(jQuery);
