25dfca38c82caf4d3f47175c5ac80ca1e98e41a5
Benedict <benedict_wallis@yahoo.co.uk>
2017-03-31 09:32:39 +0100

    Magnifc JS updated

--- js/src/custom/magnific.js
+++ js/src/custom/magnific.js
@@ -8,6 +8,17 @@
         $('a.image-popup').magnificPopup({
             type: 'image'
         });
+
+        /**
+         * Create gallery from magnific items
+         *
+         */
+        $('.magnific-item').magnificPopup({
+            type: 'image',
+            gallery:{
+                enabled:true
+            }
+        });
     });
 
 })(jQuery);
