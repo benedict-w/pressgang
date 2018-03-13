3770358b384f5a6e130a1cb1434d31257f0c2b4a
Benedict <benedict_wallis@yahoo.co.uk>
2018-03-13 11:28:49 +0000

    Woocommerce 3.3.3 Update Fixes

--- core/site.php
+++ core/site.php
@@ -70,6 +70,7 @@ class Site extends \TimberSite
      */
     public function add_to_context($context) {
         $context['site'] = $this;
+
         $context = \apply_filters("site_context", $context);
         return $context;
     }
@@ -88,8 +89,8 @@ class Site extends \TimberSite
 
         $context['my_account_link'] = get_permalink($account_page_id);
         $context['logout_link'] = wp_logout_url(get_permalink($account_page_id));
-        $context['cart_link'] = $woocommerce->cart->get_cart_url();
-        $context['checkout_link'] = $woocommerce->cart->get_checkout_url();
+        $context['cart_link'] = wc_get_cart_url();
+        $context['checkout_link'] = wc_get_checkout_url();
         $context['cart_contents'] = $woocommerce->cart->cart_contents;
 
         return $context;
