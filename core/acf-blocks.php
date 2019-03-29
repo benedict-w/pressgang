84f91cb1bb9e45359f3b554e0ce69770fcc96f27
Benedict <benedict_wallis@yahoo.co.uk>
2019-03-29 15:03:56 +0000

    Fix for weird Gutenberg bug with wpautop
    https://github.com/WordPress/gutenberg/issues/12646

--- core/acf-blocks.php
+++ core/acf-blocks.php
@@ -19,6 +19,7 @@ class AcfBlocks
     {
         add_action('acf/init', array($this, 'setup'));
         add_filter('block_categories', array($this, 'add_custom_categories'));
+        add_filter('render_block', array($this, 'disable_wpautop'), 20, 2);
     }
 
     /**
@@ -63,6 +64,26 @@ class AcfBlocks
         return array_merge($categories, $this->custom_categories);
 
     }
+
+    /**
+     * disable_wpautop
+     *
+     * Try to disable wpautop inside specific blocks.
+     *
+     * @link https://wordpress.stackexchange.com/q/321662/26317
+     *
+     * @param string $block_content The HTML generated for the block.
+     * @param array  $block The block.
+     */
+    public function disable_wpautop( $block_content, $block )
+    {
+        if (in_array(preg_replace('/^acf\//','', $block['blockName']), array_keys(Config::get('acf-blocks')))) {
+            \remove_filter('the_content', 'wpautop');
+        }
+
+        return $block_content;
+    }
+
 }
 
 new AcfBlocks();
