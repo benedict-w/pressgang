f7a8847fa296c5ebf3c23045a954b5b1d5262e45
Benedict <ben@benedict-wallis.com>
2019-07-03 12:42:24 +0100

    Added Bootstrap dropdown hover JS

--- js/src/custom/dropdown-hover.js
+++ js/src/custom/dropdown-hover.js
@@ -10,17 +10,30 @@
         var toggleDropdown = function(e) {
             var _d = $(e.target).closest('.dropdown'),
                 _m = $('.dropdown-menu', _d);
-            setTimeout(function(){
-                const shouldOpen = e.type !== 'click' && _d.is(':hover');
-                _m.toggleClass('show', shouldOpen);
-                _d.toggleClass('show', shouldOpen);
-                $('[data-toggle="dropdown"]', _d).attr('aria-expanded', shouldOpen);
-            }, e.type === 'mouseleave' ? 300 : 0);
+
+            if (window.outerWidth > 991) {
+                setTimeout(function () {
+                    const shouldOpen = e.type !== 'click' && _d.is(':hover');
+                    _m.toggleClass('show', shouldOpen);
+                    _d.toggleClass('show', shouldOpen);
+                    $('[data-toggle="dropdown"]', _d).attr('aria-expanded', shouldOpen);
+                }, e.type === 'mouseleave' ? 300 : 0);
+            }
         };
 
         $('body')
             .on('mouseenter mouseleave','.dropdown',toggleDropdown)
             .on('click', '.dropdown-menu a', toggleDropdown);
+
+
+        $('.dropdown-toggle').on('click', function (e) {
+            if (window.outerWidth > 991) {
+                var href = $(this).prop('href');
+                if (href !== '#') {
+                    window.location.href = href;
+                }
+            }
+        })
 
     });
 
