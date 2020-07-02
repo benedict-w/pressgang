c65647f64b7b769ddc4a2c760832f91d9094bb7e
Benedict <ben@benedict-wallis.com>
2020-07-02 12:13:06 +0100

    Changed cart_contents to cart_contents_count

--- core/site.php
+++ core/site.php
@@ -112,7 +112,7 @@ class Site extends \TimberSite
         $context['logout_link'] = wp_logout_url(get_permalink($account_page_id));
         $context['cart_link'] = wc_get_cart_url();
         $context['checkout_link'] = wc_get_checkout_url();
-        $context['cart_contents'] = $woocommerce->cart->cart_contents;
+        $context['cart_contents_count'] = WC()->cart->get_cart_contents_count();
 
         return $context;
     }
