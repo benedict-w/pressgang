0d82a20503d6fb71bb67c99e81b544fe8b681dc6
Benedict <ben@benedict-wallis.com>
2020-07-03 13:28:45 +0100

    Updated jquery and jquery migrate

--- core/scripts.php
+++ core/scripts.php
@@ -114,10 +114,10 @@ class Scripts {
         if (!is_admin()) {
             add_action('wp_enqueue_scripts', function() {
                 wp_deregister_script('jquery-core');
-                wp_register_script('jquery-core', "https://code.jquery.com/jquery-3.4.1.min.js", array(), '3.4.1');
+                wp_register_script('jquery-core', "https://code.jquery.com/jquery-3.5.1.min.js", array(), '3.5.1');
 
                 wp_deregister_script( 'jquery-migrate' );
-                wp_register_script('jquery-migrate', "https://code.jquery.com/jquery-migrate-3.1.0.min.js", array(), '3.1.0');
+                wp_register_script('jquery-migrate', "https://code.jquery.com/jquery-migrate-3.3.1.min.js", array(), '3.3.1');
             }, 0);
         }
     }
