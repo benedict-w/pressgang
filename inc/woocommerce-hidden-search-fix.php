1b02c613db13c588309504600a34cc67409fd627
Benedict <ben@benedict-wallis.com>
2022-12-21 14:55:17 +0000

    Updates hidden search fix

--- inc/woocommerce-hidden-search-fix.php
+++ inc/woocommerce-hidden-search-fix.php
@@ -4,36 +4,38 @@ namespace PressGang;
 
 class WooCommerceHiddenSearchFix
 {
-    /**
-     * WooCommerceHiddenSearchFix constructor
-     *
-     */
-    public function __construct()
-    {
-        add_action('pre_get_posts', array($this, 'hidden_product_search_query_fix'));
-    }
+	/**
+	 * WooCommerceHiddenSearchFix constructor
+	 *
+	 */
+	public function __construct()
+	{
+		add_action('pre_get_posts', array($this, 'hidden_product_search_query_fix'));
+	}
 
-    /**
-     * hidden_product_search_query_fix
-     *
-     * @param bool $query
-     */
-    public function hidden_product_search_query_fix( $query = false ) {
+	/**
+	 * hidden_product_search_query_fix
+	 *
+	 * @param bool $query
+	 */
+	public function hidden_product_search_query_fix( $query = false ) {
 
-        if(!is_admin() && is_search()){
+		global $wp_the_query;
 
-            $query->set('tax_query', array(
-                'relation' => 'OR',
-                array(
-                    'taxonomy' => 'product_visibility',
-                    'field'    => 'name',
-                    'terms'    => 'exclude-from-search',
-                    'operator' => 'NOT IN',
-                ),
-            ));
+		if($query === $wp_the_query && $query->is_search() && !is_admin()) {
 
-        }
-    }
+			$query->set('tax_query', array(
+				'relation' => 'OR',
+				array(
+					'taxonomy' => 'product_visibility',
+					'field'    => 'name',
+					'terms'    => 'exclude-from-search',
+					'operator' => 'NOT IN',
+				),
+			));
+
+		}
+	}
 }
 
 new WooCommerceHiddenSearchFix();
